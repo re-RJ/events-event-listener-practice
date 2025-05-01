89ef78c0efa5f86dbb2bf586dff08bf5e71832b7
rj_m <rjmarte@sbcglobal.net>
2025-05-01 12:15:27 -0700

    Exercise 1; mouseovers.

---
+++ js/script.js
@@ -0,0 +1,17 @@
+////Exercise #1////
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+
+fans.addEventListener("mouseover", function() {
+    fans.innerText = "123k";
+});
+
+fish.addEventListener("mouseover", function() {
+    fish.innerText = "47k";
+})
+
+;pets.addEventListener("mouseover", function() {
+    pets.innerText = "20k";
+});
+
