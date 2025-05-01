cd9bac1daba01d3e162302418acaa73a4fcb2e55
rj_m <rjmarte@sbcglobal.net>
2025-05-01 12:30:27 -0700

    Light & Dark mode switch toggleable.

--- js/script.js
+++ js/script.js
@@ -15,3 +15,21 @@ fish.addEventListener("mouseover", function() {
     pets.innerText = "20k";
 });
 
+
+////Exercise #2////
+var toggle = document.querySelector("#toggle");
+var ball = document.querySelector(".ball");
+var quincyMode = document.querySelector("h1");
+var body = document.querySelector("body");
+
+toggle.addEventListener("click", function() {
+    if (body.classList.contains("light")){
+        body.classList.remove("light");
+        ball.classList.remove("move-right");
+        quincyMode.innerText = "Stealth Quincy";
+    } else {
+        body.classList.add("light");
+        ball.classList.add("move-right");
+        quincyMode.innerText = "Party Quincy";
+    }
+});
